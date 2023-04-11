27ebef1acb19f4041ea0b4243cc03fb61efa023e
arjansunar <arjan.gahatrajsunar@gmail.com>
2023-04-11 13:41:18 +0545

    adding manual invocation and mapping over objects example

--- src/page/Form.tsx
+++ src/page/Form.tsx
@@ -6,42 +6,31 @@ import { useForm } from "antd/es/form/Form";
 
 /* proposed asyncoptions*/
 type AsyncOptionsProps = {
-  children?: (data: any[]) => ReactNode[];
+  children: (data: any[]) => ReactNode[];
   queryFn: () => Promise<any[]>;
+  formItemProps: FormItemProps;
 };
 
 const { Option } = Select;
-const AsyncOption = ({ queryFn, children }: AsyncOptionsProps) => {
+/**
+ * Wrapper around AndD's `Form.Item` with a `Select` component to render Select.Options
+ * according to the response of an `Api request`
+ *
+ * WARN: It consists its own `Form.Item` component so do not wrap it with another `Form.Item`
+ * component
+ */
+const AsyncOption = ({
+  queryFn,
+  formItemProps,
+  children,
+}: AsyncOptionsProps) => {
   const { data, isLoading } = useQuery({
     queryFn: queryFn,
   });
-  if (isLoading || !data) return <Select loading />;
-
-  if (!children)
-    return (
-      <Form.Item
-        label="AsyncOption"
-        name="AsyncOption"
-        rules={[{ required: true, message: "Please select a username!" }]}
-      >
-        <Select loading={isLoading}>
-          {data &&
-            data.map((item) => (
-              <Option key={item.id} value={item.name}>
-                {item.name}
-              </Option>
-            ))}
-        </Select>
-      </Form.Item>
-    );
 
   //INFO: must have the Form.Item must be coupled with Select component
   return (
-    <Form.Item
-      label="AsyncOption"
-      name="AsyncOption"
-      rules={[{ required: true, message: "Please select a username!" }]}
-    >
+    <Form.Item {...formItemProps}>
       <Select loading={isLoading}>{data && children(data)}</Select>
     </Form.Item>
   );
@@ -51,7 +40,7 @@ const AsyncOption = ({ queryFn, children }: AsyncOptionsProps) => {
 interface CustomFormItemsProps extends FormItemProps {
   key: Key;
   name: FormItemProps["name"];
-  element?: ReactNode;
+  render?: (itemProps: FormItemProps) => ReactNode;
   asyncComp?: true;
 }
 
@@ -61,7 +50,7 @@ const formItems: CustomFormItemsProps[] = [
     label: "Default",
     name: "select",
     rules: [{ required: true, message: "this is required" }],
-    element: (
+    render: () => (
       <Select>
         <Select.Option value="Demo">Demo</Select.Option>
       </Select>
@@ -72,8 +61,8 @@ const formItems: CustomFormItemsProps[] = [
     label: "AsyncOption",
     name: "asyncOption",
     asyncComp: true,
-    element: (
-      <AsyncOption queryFn={getAllPayplanName}>
+    render: (formItemProps) => (
+      <AsyncOption queryFn={getAllPayplanName} formItemProps={formItemProps}>
         {(data) =>
           data.map((item) => (
             <Option key={item.id} value={item.id}>
@@ -95,31 +84,36 @@ export const FormWithOptions: React.FC = () => {
 
   return (
     <Form
-      name="basic"
-      initialValues={{ remember: true }}
       form={form}
       labelCol={{ span: 4 }}
-      wrapperCol={{ span: 14 }}
       layout="horizontal"
       style={{ maxWidth: 600 }}
       onFinish={handleFormSubmit}
     >
-      {formItems.map(({ element, asyncComp, ...itemProps }) => {
-        if (asyncComp) return element;
+      {/* call by mapping over a json object */}
+      {formItems.map(({ render, asyncComp, ...formItemProps }) => {
+        if (asyncComp && render)
+          return <div key={formItemProps.key}> {render(formItemProps)}</div>;
         return (
-          <Form.Item {...itemProps}>{element ? element : <Input />}</Form.Item>
+          <Form.Item {...formItemProps}>
+            {render ? render(formItemProps) : <Input />}
+          </Form.Item>
         );
       })}
-      {/* manual form call */}
-      {/* <AsyncOption queryFn={getAllPayplanName}> */}
-      {/*   {(data) => */}
-      {/*     data.map((item) => ( */}
-      {/*       <Option key={item.id} value={item.id}> */}
-      {/*         {item.name} */}
-      {/*       </Option> */}
-      {/*     )) */}
-      {/*   } */}
-      {/* </AsyncOption> */}
+      {/* manual invokation */}
+      <AsyncOption
+        queryFn={getAllPayplanName}
+        formItemProps={{ name: "ManualAsync", label: "Manual", required: true }}
+      >
+        {(payplanList) =>
+          payplanList.map((payplan) => (
+            <Option key={payplan.id} value={payplan.name}>
+              {payplan.name}
+            </Option>
+          ))
+        }
+      </AsyncOption>
+
       {/* submit button */}
       <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
         <Button type="primary" htmlType="submit">
